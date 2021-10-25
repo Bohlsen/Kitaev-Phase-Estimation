949ca57deda422f98b0b9888442d208e41e777cc
Bohlsen <79351569+Bohlsen@users.noreply.github.com>
2021-10-25 13:52:17 +1100

    Create Host.py

---
+++ Host.py
@@ -0,0 +1,23 @@
+import qsharp
+import Kitaev
+import numpy as np
+
+Check_Message = Kitaev.Import_check.simulate()
+print(Check_Message)
+
+if __name__ == '__main__':
+    for i in range(5):
+        Rz_phi = Kitaev.TestRZ.simulate()
+        print(i," TestRZ : ",Rz_phi)
+
+    for i in range(5):
+        S_Phi = Kitaev.TestS0.simulate()
+        print(i," TestS0 : ",S_Phi)
+
+    for i in range(5):
+        S_Phi = Kitaev.TestS1.simulate()
+        print(i," TestS1 : ",S_Phi)
+        
+    for i in range(5):
+        H_Phi = Kitaev.TestH.simulate()
+        print(i," TestH : ",H_Phi)
